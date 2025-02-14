51760781bba39fa6d5ec9fc3c9ce7a951637aa61
Florian Finke <flo@florianfinke.de>
2025-02-14 17:00:51 +0100

    feat: disable gemini

---
+++ nvim/lua/plugins/gemini.lua
@@ -0,0 +1,9 @@
+return {
+  --   "kiddos/gemini.nvim",
+  --   config = function()
+  --     require("gemini").setup({
+  --       hints = { hints_delay = 1000, insert_result_key = "<M-CR>" },
+  --       completion = { insert_result_key = "<M-CR>" },
+  --     })
+  --   end,
+}
